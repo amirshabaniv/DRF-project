ef3550c1ea1445a0e8851cd99400611f2b9936f8
Amir Shabani <129328075+amirshabaniv@users.noreply.github.com>
2024-04-16 02:22:48 +0330

    Update models.py

--- downloads/models.py
+++ downloads/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-from products.models import Category
 
+
+class NewsCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'News Category'
+        verbose_name_plural = 'News Categories'
+
+    def __str__(self):
+        return self.name
 
 class Download(models.Model):
     title = models.CharField(max_length=100)
@@ -8,7 +17,7 @@ class Download(models.Model):
     description = models.TextField()
     file = models.FileField(upload_to='files/download_files', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='downloads')
+    category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE, related_name='downloads')
 
     def __str__(self):
         return self.title
